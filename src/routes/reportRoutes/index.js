7643657bd787f9c64b192f61ffe7e08e23aa61ac
jaime <jgarciadiaz@groupon.com>
2018-12-26 17:58:55 -0600

    feat: add 60 secs cache to get report ednpoint

--- src/routes/reportRoutes/index.js
+++ src/routes/reportRoutes/index.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const debug = require('debug')('reportRoutes');
 const cors = require('cors');
+const redis = require('redis');
 
 const router = express.Router(); // eslint-disable-line
+const client = redis.createClient();
 
 const PortModel = require('../../models/portModel');
 
 router.get('/', cors(), async (req, res) => {
   const city = req.param('city');
   if (city) {
-    const report = await PortModel.find({
-      city,
-    })
-    .sort({
-      created: -1,
-    })
-    .limit(1);
+    const cacheKey = `report:${city}`;
+    client.get(cacheKey, async (err, cacheReport) => {
+      let report = null;
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(report));
+      if (err || !cacheReport) {
+        const data = await PortModel.find({
+          city,
+        })
+        .sort({
+          created: -1,
+        })
+        .limit(1);
+
+        report = JSON.stringify(data);
+
+        client.set(cacheKey, report, 'EX', 60);
+      } else {
+        report = cacheReport;
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.send(report);
+    });
   } else {
     debug(`city not found: ${city}`);
     res.sendStatus(500);
