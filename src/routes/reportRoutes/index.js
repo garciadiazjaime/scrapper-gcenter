6f007161da8921d74cf6decc15864c92f281b99f
jaime <jgarciadiaz@groupon.com>
2018-12-27 23:41:27 -0600

    revert: remove redis

--- src/routes/reportRoutes/index.js
+++ src/routes/reportRoutes/index.js
@@ -1,39 +1,24 @@
 const express = require('express');
 const debug = require('debug')('reportRoutes');
 const cors = require('cors');
-const redis = require('redis');
 
 const router = express.Router(); // eslint-disable-line
-const client = redis.createClient();
 
 const PortModel = require('../../models/portModel');
 
 router.get('/', cors(), async (req, res) => {
   const city = req.param('city');
   if (city) {
-    const cacheKey = `report:${city}`;
-    client.get(cacheKey, async (err, cacheReport) => {
-      let report = null;
+    const report = await PortModel.find({
+      city,
+    })
+    .sort({
+      created: -1,
+    })
+    .limit(1);
 
-      if (err || !cacheReport) {
-        const data = await PortModel.find({
-          city,
-        })
-        .sort({
-          created: -1,
-        })
-        .limit(1);
-
-        report = JSON.stringify(data && data[0]);
-
-        client.set(cacheKey, report, 'EX', 60);
-      } else {
-        report = cacheReport;
-      }
-
-      res.setHeader('Content-Type', 'application/json');
-      res.send(report);
-    });
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(report && report[0]));
   } else {
     debug(`city not found: ${city}`);
     res.sendStatus(500);
