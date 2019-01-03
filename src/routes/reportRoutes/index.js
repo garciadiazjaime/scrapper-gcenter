1cd64c680fd427a5a7f25e22aad431ae0d68de42
jaime <jgarciadiaz@groupon.com>
2019-01-03 16:06:16 -0600

    feat: start weekly report

--- src/routes/reportRoutes/index.js
+++ src/routes/reportRoutes/index.js
@@ -25,4 +25,48 @@ router.get('/', cors(), async (req, res) => {
   }
 });
 
+router.get('/last-7-days', cors(), async (req, res) => {
+  const city = req.param('city');
+  if (city) {
+    const startDay = new Date();
+    startDay.setDate(startDay.getDate() - 7);
+    const query = {
+      city,
+      created: {
+        $gte: startDay.toISOString(),
+      },
+    };
+
+    const report = await PortModel
+      .find(query)
+      .sort({
+        created: -1,
+      });
+
+    const data = report.reduce((accumulator, item) => {
+      const date = new Date(item.created);
+      const dayKey = date.getDate();
+
+      if (!accumulator[dayKey]) {
+        accumulator[dayKey] = {};
+      }
+
+      const hourKey = date.getHours();
+      if (!accumulator[dayKey][hourKey]) {
+        accumulator[dayKey][hourKey] = [];
+      }
+
+      accumulator[dayKey][hourKey].push(item);
+
+      return accumulator;
+    }, {});
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data));
+  } else {
+    debug(`city not found: ${city}`);
+    res.sendStatus(500);
+  }
+});
+
 module.exports = router;
